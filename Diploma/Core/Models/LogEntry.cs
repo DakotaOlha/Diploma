0ee7fdfc4ec0ddcd9e2d489480b61057e3e75903
DakotaOlha <ochaiuk2006@gmail.com>
2026-04-02 16:40:48 +0300

    feat: added model for logs

--- Diploma/Core/Models/LogEntry.cs
+++ Diploma/Core/Models/LogEntry.cs
@@ -2,5 +2,11 @@
 
 public class LogEntry
 {
-    
+    public int Id { get; set; }
+    public int SessionId { get; set; }
+    public DateTime Timestamp { get; set; }
+    public TimeSpan Offset { get; set; }      // time since the start of recording
+    public string EventType { get; set; }     // "FILE_SAVE", "IDE_OPEN", etc
+    public string Description { get; set; }
+    public string? Metadata { get; set; } 
 }
