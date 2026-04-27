7cfef8138919e8452d0a59bab8a7def29920ef0b
DakotaOlha <ochaiuk2006@gmail.com>
2026-04-27 12:27:33 +0300

    feat: Added new event types and implement service to MainViewModel

--- Diploma/Core/Models/EventTypes.cs
+++ Diploma/Core/Models/EventTypes.cs
@@ -11,4 +11,8 @@ public static class EventTypes
     public const string IdleEnd         = "IDLE_END";
     public const string ManualMarker    = "MANUAL_MARKER";
     public const string Screenshot      = "SCREENSHOT";
+    public const string IdeOpened       = "IDE_OPENED";
+    public const string IdeClosed       = "IDE_CLOSED";
+    public const string FileSwitched    = "FILE_SWITCHED";
+    public const string FileSavedAuto   = "FILE_SAVED_AUTO";
 }
