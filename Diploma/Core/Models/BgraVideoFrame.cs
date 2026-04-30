22ce8c5a3e91e411cfe7010fbd10f4aef9dbddb8
DakotaOlha <ochaiuk2006@gmail.com>
2026-04-30 15:19:36 +0300

    feat: Added an ArrayPool for byte[] frames

--- Diploma/Core/Models/BgraVideoFrame.cs
+++ Diploma/Core/Models/BgraVideoFrame.cs
@@ -1,29 +1,37 @@
-﻿using System.IO;
+﻿using System.Buffers;
+using System.IO;
 using FFMpegCore.Pipes;
 
 namespace Diploma.Core.Models;
 
 public class BgraVideoFrame : IVideoFrame
 {
-    public int Width { get; }
+    public int Width  { get; }
     public int Height { get; }
     public string Format => "bgra";
+
     private readonly byte[] _data;
+    private readonly int    _length;
+    private readonly bool   _pooled;
 
-    public BgraVideoFrame(byte[] data, int width, int height)
+    public BgraVideoFrame(byte[] data, int width, int height, bool pooled = false)
     {
-        _data = data;
-        Width = width;
-        Height = height;
-    }
-
-    public void Serialize(Stream pipe)
-    {
-        pipe.Write(_data, 0, _data.Length);
+        _data   = data;
+        Width   = width;
+        Height  = height;
+        _length = width * height * 4;
+        _pooled = pooled;
     }
     
+    public void Serialize(Stream pipe)
+        => pipe.Write(_data, 0, _length);
+
     public Task SerializeAsync(Stream pipe, CancellationToken token)
+        => pipe.WriteAsync(_data, 0, _length, token);
+
+    public void Return()
     {
-        return pipe.WriteAsync(_data, 0, _data.Length, token);
+        if (_pooled)
+            ArrayPool<byte>.Shared.Return(_data);
     }
 }
