014a719c8524eb17274ddf76dfc852606291551a
DakotaOlha <ochaiuk2006@gmail.com>
2026-05-01 16:07:31 +0300

    feat: created tray-icon

--- Diploma/App.xaml.cs
+++ Diploma/App.xaml.cs
@@ -6,6 +6,7 @@ using Diploma.Data.Database;
 using Diploma.Helpers;
 using Diploma.ViewModels;
 using Diploma.Views;
+using Hardcodet.Wpf.TaskbarNotification;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
@@ -14,29 +15,27 @@ namespace Diploma;
 
 public partial class App : Application
 {
+    private IHost _host = null!;
+    private TaskbarIcon? _trayIcon;
 
-    private IHost _host;
-    
     protected override async void OnStartup(StartupEventArgs e)
     {
         Log.Logger = new LoggerConfiguration()
             .WriteTo.File("logs/algoreplay.log", rollingInterval: RollingInterval.Day)
             .CreateLogger();
-        
+
         _host = Host.CreateDefaultBuilder()
             .UseSerilog()
-            .ConfigureServices((context, services) =>
+            .ConfigureServices((_, services) =>
             {
-                var appData = Environment.GetFolderPath(
-                    Environment.SpecialFolder.ApplicationData);
-                var dbPath = Path.Combine(appData, "AlgoReplay", "data.db");
+                var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                var dbPath  = Path.Combine(appData, "AlgoReplay", "data.db");
                 var connStr = $"Data Source={dbPath}";
 
                 var dbInit = new DatabaseInitializer(connStr);
                 dbInit.Initialize();
-                
-                services.AddSingleton<ILogService>(_ => new LogService(connStr));
 
+                services.AddSingleton<ILogService>(_ => new LogService(connStr));
                 services.AddSingleton<IInputMonitorService, InputMonitorService>();
                 services.AddSingleton<IAudioCaptureService, AudioCaptureService>();
                 services.AddSingleton<IScreenCaptureService, ScreenCaptureService>();
@@ -54,8 +53,10 @@ public partial class App : Application
                 services.AddSingleton<MainWindow>();
             })
             .Build();
-        
+
         await _host.StartAsync();
+
+        _trayIcon = (TaskbarIcon)FindResource("TrayIcon");
 
         ShutdownMode = ShutdownMode.OnExplicitShutdown;
 
@@ -76,28 +77,55 @@ public partial class App : Application
         {
             splash.Close();
         }
-        
+
         var mainWindow = _host.Services.GetRequiredService<MainWindow>();
-
-        ShutdownMode = ShutdownMode.OnMainWindowClose;
-        MainWindow   = mainWindow;
-
+        MainWindow = mainWindow;
         mainWindow.Show();
-        
+
         var hotkeyService = _host.Services.GetRequiredService<IGlobalHotkeyService>();
         hotkeyService.Start();
-        
+
         base.OnStartup(e);
     }
-    
-    public OverlayWindow GetOverlay() => 
+
+    private void TrayIcon_DoubleClick(object sender, RoutedEventArgs e) => ShowMainWindow();
+
+    private void TrayOpen_Click(object sender, RoutedEventArgs e) => ShowMainWindow();
+
+    private void TrayStartStop_Click(object sender, RoutedEventArgs e)
+    {
+        var vm = _host.Services.GetRequiredService<MainViewModel>();
+        if (vm.IsRecording)
+            _ = vm.StopRecordingCommand.ExecuteAsync(null);
+        else
+            _ = vm.StartRecordingCommand.ExecuteAsync(null);
+    }
+
+    private void TrayExit_Click(object sender, RoutedEventArgs e)
+    {
+        _trayIcon?.Dispose();
+        Shutdown();
+    }
+
+    private void ShowMainWindow()
+    {
+        var win = _host.Services.GetRequiredService<MainWindow>();
+        win.Show();
+        if (win.WindowState == System.Windows.WindowState.Minimized)
+            win.WindowState = System.Windows.WindowState.Normal;
+        win.Activate();
+    }
+
+    public OverlayWindow GetOverlay() =>
         _host.Services.GetRequiredService<OverlayWindow>();
 
     protected override async void OnExit(ExitEventArgs e)
     {
+        _trayIcon?.Dispose();
+
         var player = _host.Services.GetRequiredService<PlayerViewModel>();
         player.Dispose();
-        
+
         await _host.StopAsync();
         Log.CloseAndFlush();
         base.OnExit(e);
--- Diploma/Views/MainWindow.xaml.cs
+++ Diploma/Views/MainWindow.xaml.cs
@@ -1,21 +1,33 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
 using Diploma.ViewModels;
 
 namespace Diploma.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow(MainViewModel viewModel, SessionsView sessionsView, PlayerView playerView, SessionsViewModel sessionsViewModel)
+    public MainWindow(
+        MainViewModel viewModel,
+        SessionsView sessionsView,
+        PlayerView playerView,
+        SessionsViewModel sessionsViewModel)
     {
         InitializeComponent();
         DataContext = viewModel;
-    
+
         SessionsTab.Content = sessionsView;
-        PlayerTab.Content = playerView;
-    
+        PlayerTab.Content   = playerView;
+
         sessionsViewModel.NavigateToPlayer += () =>
         {
             PlayerTab.IsSelected = true;
         };
+    }
+
+    protected override void OnClosing(CancelEventArgs e)
+    {
+        e.Cancel = true;
+        Hide();
+        base.OnClosing(e);
     }
 }
