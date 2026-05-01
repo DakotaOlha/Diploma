3dad448981395e18182f9eb4a6426978a2bf5068
DakotaOlha <ochaiuk2006@gmail.com>
2026-05-01 10:52:32 +0300

    feat: created normal highlighting logs

---
+++ Diploma/Helpers/IsActiveEntryConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Diploma.Helpers;
+
+public sealed class IsActiveEntryConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        => values.Length == 2 && values[0] is not null && ReferenceEquals(values[0], values[1]);
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+}
