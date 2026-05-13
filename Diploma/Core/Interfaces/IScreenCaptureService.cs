6ccbaca1b9320d5a2ab2b4979027864812dd6d36
DakotaOlha <ochaiuk2006@gmail.com>
2026-05-13 13:25:30 +0300

    fix: fixed an issue with the order of operations in StartRecordingAsync

--- Diploma/Core/Interfaces/IScreenCaptureService.cs
+++ Diploma/Core/Interfaces/IScreenCaptureService.cs
@@ -6,15 +6,14 @@ namespace Diploma.Core.Interfaces;
 public interface IScreenCaptureService
 {
     bool IsRecording { get; }
-
-    Task<bool> StartAsync(string outputPath, CancellationToken ct = default);
-    Task<string?> TakeScreenshotAsync(string outputDir);
+    Task PrepareAsync(string outputPath, CancellationToken ct = default);
+    Task BeginCaptureAsync(CancellationToken ct = default);
     Task StopAsync();
-
+    Task<string?> TakeScreenshotAsync(string outputDir);
     BitmapSource? GetLatestFrameAsBitmap();
 
-    event EventHandler<string>? StatusChanged;
-    event EventHandler? RecordingStarted;
-    event EventHandler? CaptureTargetSelected;
+    event EventHandler<string>?             StatusChanged;
+    event EventHandler?                     RecordingStarted;      
+    event EventHandler?                     CaptureTargetSelected; 
     event EventHandler<DropStatsEventArgs>? DropStatsChanged;
 }
